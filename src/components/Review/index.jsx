f74fdd6444d8f9b50fccf0ab070de22bae638232
Max Kuh <maxes2805582@gmail.com>
2023-06-04 22:57:12 +0300

    Add solution for ResponeData

--- src/components/Review/index.jsx
+++ src/components/Review/index.jsx
@@ -1,26 +1,34 @@
-
 import './style.css';
+import React from 'react';
 function Review(props) {
-  const {name} = props;
-  const {  title, mark, text, authorName, authorImg } = name
-  return (
-    <div className="commentsContainer">
-      <li class="commentWrapper">
-        <article className="commentArticle">
-          <div className="authorData">
-            <div className="imageWrapper">
-              <img className="authorImg" src={authorImg} alt={authorName} />
-            </div>
-            <p className="authorName">{authorName}</p>
-          </div>
-          <div className="commentBody">
-            <h2 className="commentTitle">{title}</h2>
-            <p className="mark">{mark}</p>
-            <p className="commentText">{text}</p>
-          </div>
-        </article>
-      </li>
-    </div>
+  const { name } = props;
+  const fragment = React.createElement(
+    React.Fragment,
+    null,
+    name.map((value) => {
+      const { title, mark, text, authorName, authorImg } = value;
+      return (
+        <div className="commentsContainer">
+          <li class="commentWrapper">
+            <article className="commentArticle">
+              <div className="authorData">
+                <div className="imageWrapper">
+                  <img className="authorImg" src={authorImg} alt={authorName} />
+                </div>
+                <p className="authorName">{authorName}</p>
+              </div>
+              <div className="commentBody">
+                <h2 className="commentTitle">{title}</h2>
+                <p className="mark">{mark}</p>
+                <p className="commentText">{text}</p>
+              </div>
+            </article>
+          </li>
+        </div>
+      );
+    })
   );
+
+  return fragment;
 }
 export default Review;
